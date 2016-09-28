9c45c3fd813e0632fd2eb05e23b5eeb442062125
Chuan Yu <chuanyu@Chuans-MacBook-Air.local>
2016-09-28 16:55:52 +0800

    All but Four Square are working

--- js/app.js
+++ js/app.js
@@ -21,65 +21,115 @@ ko.bindingHandlers.googlemap = {
 
         function searchCallback(results, status) {
             if (status == google.maps.places.PlacesServiceStatus.OK) {
-                var placeMarkers = createPlaceMarkers(results, map);
-                viewModel.placeMarkers(placeMarkers);
+                viewModel.allPlaces(results);
             }
         }
     }
 };
 
-function createPlaceMarkers(places, map) {
-    var placeMarkers = [];
-    for (var i = 0; i < places.length; i++) {
-        var location = places[i].geometry.location;
-        var marker = new google.maps.Marker({
-            position: location,
-            map: map
-        });
-        placeMarkers.push([places[i], marker]);
+function clearMarkers(placeMarkers) {
+    var placeMarkers = placeMarkers();
+    for (var i = 0; i < placeMarkers.length; i++) {
+        placeMarkers[i][1].setMap(null);
     }
-    return placeMarkers;
-}
-
-function createInfoWindow(placeMarker, map) {
-    var place = placeMarker()[0];
-    var marker = placeMarker()[1];
-
-    console.log(place.name);
-
-    var contentString = '<div id="content">' +
-        '<div id="siteNotice">' +
-        '<h5>' + place.name + '</h5>' +
-        '<p>' + place.vicinity + '</h5>' +
-        '</div>' +
-        '</div>';
-
-    var infoWindow = new google.maps.InfoWindow({
-        content: contentString
-    });
-
-    infoWindow.open(map, marker);
-
-    return infoWindow;
 }
 
 function MapViewModel() {
+
+    //Data
+
     var self = this;
-    var currentInfoWindow = null;
+    self.currentInfoWindow = null;
 
     self.map = null;
+    self.allPlaces = ko.observableArray([]);
     self.placeMarkers = ko.observableArray([]);
     self.center = ko.observable(new google.maps.LatLng(1.300527, 103.902021));
-    self.currentItem = ko.observable();
+    self.currentItem = ko.observableArray([]);
+    self.query = ko.observable("");
+    self.current4Square = ko.observable();
 
+    //Operations
 
     self.selectItem = function(item) {
-        if (currentInfoWindow) {
-            currentInfoWindow.close();
+        if (self.currentInfoWindow) {
+            self.currentInfoWindow.close();
         }
         self.currentItem(item);
-        currentInfoWindow = createInfoWindow(self.currentItem, self.map);
-        console.log(item[0]);
-        console.log(item[1]);
+        //console.log(self.currentItem());
+        self.createInfoWindow(self.currentItem);
+        //self.fourSquareSearch(self.currentItem()[0]);
     };
+
+    self.createPlaceMarkers = function(places){
+        var places = ko.unwrap(places);
+        clearMarkers(self.placeMarkers);
+        self.placeMarkers.removeAll();
+        for (var i = 0; i < places.length; i++) {
+            var location = places[i].geometry.location;
+            var marker = new google.maps.Marker({
+                position: location,
+                map: self.map
+            });
+            self.placeMarkers.push([places[i], marker]);
+        }
+    };
+
+    self.createInfoWindow = function(placeMarker) {
+        var place = placeMarker()[0];
+        var marker = placeMarker()[1];
+        var contentString = '<div id="content">' +
+            '<div id="siteNotice">' +
+            '<h5>' + place.name + '</h5>' +
+            '<p>' + place.vicinity + '</h5>' +
+            '<p id="foursquare" data-bind="text: $root.current4Square"></p>'
+            '</div>' +
+            '</div>';
+
+        // self.currentInfoWindow = new google.maps.InfoWindow();
+        self.currentInfoWindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+
+        //self.currentInfoWindow.setContent(document.getElementById('info-window-container'));
+
+        self.currentInfoWindow.open(self.map, marker);
+    };
+
+    self.search = function(q) {
+        var allPlaces = self.allPlaces();
+        var places = [];
+        for (var x in allPlaces) {
+            if (allPlaces[x].name.toLowerCase().indexOf(q.toLowerCase()) >= 0) {
+                places.push(allPlaces[x]);
+            }
+        }
+        console.log(places);
+        self.createPlaceMarkers(places);
+    };
+
+    self.fourSquareSearch = function(place) {
+        var location = place.geometry.location;
+        var latLng = location.lat() + ',' + location.lng();
+        var name = place.name;
+        var url = "https://api.foursquare.com/v2/venues/search?" +
+            "ll=" + latLng +
+            "&name=" + name +
+            "&near=Singapore" +
+            "&intent=match" +
+            "&v=20160928" +
+            "&client_id=5TAIH3KK4S33N0OIYGFJA3MSAIR2GF05ZVNFP3CYDPFWZQEI" +
+            "&client_secret=5H1U14GEG1Y1EU1RGBDGUDZN52GPYVHTK0F5URCXRTHWVVHS";
+        console.log(url);
+
+        $.getJSON(url, function(data) {
+            self.current4Square(data.response.venues[0].stats.checkinsCount);
+            console.log(self.current4Square())
+        })
+
+    };
+
+    //Subscriptions
+    self.allPlaces.subscribe(self.createPlaceMarkers);
+    self.query.subscribe(self.search);
 }
--- js/init.js
+++ js/init.js
@@ -2,3 +2,13 @@
 function init() {
     ko.applyBindings(new MapViewModel)
 };
+
+function error(serviceName) {
+    $('#error_modal').modal('show');
+    $('.error-message').text("Couldn't load " +
+        serviceName + ". " +
+        "Please check your network settings.");
+    $('.btn-refresh').click(function() {
+        location.reload();
+    });
+}

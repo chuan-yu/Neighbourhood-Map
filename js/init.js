2599fe7acb05cb3e79a33a27ae23001a9936472e
Chuan Yu <chuanyu@Chuans-MacBook-Air.local>
2016-10-05 17:48:22 +0800

    Minor change on comments

--- js/init.js
+++ js/init.js
@@ -1,14 +1,18 @@
 'use strict';
 var map;
 var center;
-// Start KnockOutJS bindings
+
 function init() {
+
+    // Create the map
     center = new google.maps.LatLng(1.300527, 103.902021);
     var options = {
         center: center,
         zoom: 16
     };
     map = new google.maps.Map(document.getElementById('map'), options);
+
+    // Apply KnockOutJS bindings
     ko.applyBindings(new MapViewModel);
 }
 
